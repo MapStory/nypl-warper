b5177ecc238005dc734b53d60d5fc5d2f7d5bf5f
David J. Lee <david@lee.dj>
2017-07-12 21:40:09 -0500

    stub out cap deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,8 @@
 # config valid only for Capistrano 3.1
 lock '3.2.1'
 
-set :application, 'mapwarper'
-set :repo_url, 'git@github.com:timwaters/mapwarper.git'
+set :application, 'warper'
+set :repo_url, 'git@github.com:MapStory/warper.git'
 
 # Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
@@ -10,12 +10,11 @@ set :branch, "master"
 
 # Default deploy_to directory is /var/www/my_app
 # set :deploy_to, '/var/www/my_app'
-set :deploy_via, :copy
 
 # Default value for :scm is :git
 set :scm, :git
 
-set :user, "tim"
+set :user, "ubuntu"
 
 # Default value for :format is :pretty
 set :format, :pretty
@@ -37,7 +36,7 @@ set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public
 set :rails_env, "production"
 
 # Default value for keep_releases is 5
-set :keep_releases, 5
+set :keep_releases, 3
 
 namespace :deploy do
 
@@ -50,14 +49,4 @@ namespace :deploy do
   end
 
   after :publishing, :restart
-
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
-    end
-  end
-
 end

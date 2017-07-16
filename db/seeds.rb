738880b035d52a34323609e17ad3dd9e42c0ce5d
David J. Lee <david@lee.dj>
2017-07-15 22:36:22 -0500

    add seed to create admin user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+# A default admin user. For some reason seed isn't assigning an ID?
+user = User.create(login: "warper", email: "warper@localhost", password: "password", password_confirmation: "password", confirmed_at: Time.now, description: "Dev admin account")  
+#puts user.save
+#puts user.valid?
+#puts user.inspect
+#puts user.errors.messages
+Permission.create(role: Role.find_by_name('super user'), user: user)
+Permission.create(role: Role.find_by_name('administrator'), user: user)

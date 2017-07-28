b9495f3634dbcf1e0a1a70d161f932ced5ce7f82
David J. Lee <david@lee.dj>
2017-07-27 21:19:17 -0500

    load mapscript globally

---
+++ config/initializers/mapscript.rb
@@ -0,0 +1,8 @@
+# System library as opposed to gem, so we have to load it.
+
+require 'mapscript'
+
+# For some reason the previous code had 
+# include Mapscript
+# but it always references it with the namespace, so unsure why
+# including...

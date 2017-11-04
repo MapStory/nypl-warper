686acf8e15fa64088a8ae246532578e36cee3572
David J. Lee <david@lee.dj>
2017-11-04 13:00:37 -0500

    add note about how to ignore certain tables

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,8 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+#ActiveRecord::SchemaDumper.ignore_tables = ["table_name"]
+
 module Rails4Mapwarper
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
